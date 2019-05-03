77c0e8d5125876aa568021f594ad7400c1ff33b8
Taimoor Ali <20100217@lums.edu.pk>
2019-05-04 04:54:14 +0500

    Judge Screen Completed, including Modals

--- back-end/uploads/44c96a3a-7f1a-404d-b6e9-167eb412e3c3.py
+++
@@ -1,7 +0,0 @@
-T = int(raw_input())
-for t in range(0,T):
-    #N = raw_input()
-    print("Hello W")
-
-# while(True):
-#     pass
--- back-end/uploads/83aafe28-9c07-45d5-9047-1263aa4abfc9.py
+++
@@ -1,7 +0,0 @@
-T = int(raw_input())
-for t in range(0,T):
-    N = raw_input()
-    print(N)
-
-# while(True):
-#     pass
